e635bc20dca94e79fa47805d25321ffb829f4937
Daniil12331 <bobeshkodaniil2006@gmail.com>
2025-12-03 22:13:24 +0300

    Add files via upload

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,3 +1,16 @@
+<!-- src/pages/HomePage.vue -->
 <template>
-  <div>Тут что-то будет!</div>
+  <div class="home">
+    <IpCalculator />
+  </div>
 </template>
+
+<script setup>
+import IpCalcculator from '@/components/IpCalculator.vue';
+</script>
+
+<style scoped>
+.home {
+  padding: 2rem 1rem;
+}
+</style>
